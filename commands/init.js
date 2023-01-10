c3d8d117bd173dbbbadc8aa19bac484e50e9467b
Arthur Baldoqui <arthur.b.bergamo@gmail.com>
2023-01-10 02:43:11 -0300

    initial discordjs project

---
+++ commands/init.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js');
+// const initmsg = require("../Embed/initmsg.js")
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('init')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply('Pong!');
+	},
+};
